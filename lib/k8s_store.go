c5ce1f4317105937ec6343584b4758db6c67e573
Denis Makogon <lildee1991@gmail.com>
2018-01-24 17:16:59 +0200

    Fixes

--- lib/k8s_store.go
+++ lib/k8s_store.go
@@ -8,11 +8,11 @@ import (
 	"sync"
 
 	"github.com/sirupsen/logrus"
+	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/api/core/v1"
 	"k8s.io/client-go/rest"
 )
 
